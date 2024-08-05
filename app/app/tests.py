fbda9d3a5b8a2cd0a162ebb7efaffa380aa045aa
Giacomo <giacomo.sauchelli@gmail.com>
2024-08-05 15:12:35 +0200

    lesson 35

---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests.
+"""
+from django.test import SimpleTestCase
+
+from . import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module. """
+
+    def test_add_numbers(self):
+        """ Test adding numbers together. """
+        res = calc.add(6, 6)
+
+        self.assertEqual(res, 12)
+
+    def test_subtract_numbers(self):
+        """ Test subtracting numbers. """
+        res = calc.subtract(10, 15)
+
+        self.assertEquals(res, 5)
